3be7a3885f2c008c9e263985a512106c546d5da0
JasonBian <imaginezz@outlook.com>
2016-09-05 01:11:14 +0800

    first commit

---
+++ beiyou/Library.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class Library {
+    public static void DebugOutput(object log) {
+        Debug.WriteLine(log);
+    }
+}
+class webLib {
+    public static async Task<string> HttpPost(string url, string postDataStr, string encode = "utf-8", CookieCollection cc = null) {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.CookieContainer = new CookieContainer();
+        if (cc != null) {
+            request.CookieContainer.Add(new Uri(url), cc);
+        }
+        request.Method = "POST";
+        request.Headers["Content-Length"] = Encoding.UTF8.GetByteCount(postDataStr).ToString();
+        request.ContentType = "application/x-www-form-urlencoded";
+
+        //test request header
+        //request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+        //request.Headers["Accept-Encoding"] = "gzip, deflate";
+        //request.Headers["Accept-Language"] = "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3";
+        //request.Headers["Connection"] = "keep-alive";
+        //request.Headers["DNT"] = "1";
+        //request.Headers["Host"] = "10.3.8.211";
+        //request.Headers["Referrer"] = "http://10.3.8.211/";
+        //request.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0";
+
+        Stream myRequestStream = await request.GetRequestStreamAsync();
+
+        //use this function to register the encoding machine or it will throw expect
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        //I Don't know why it doesn't work, so I use direct function to write
+        //StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding(encode));
+        //myStreamWriter.Write(postDataStr);
+        byte[] bs = Encoding.ASCII.GetBytes(postDataStr);
+        myRequestStream.Write(bs, 0, bs.Length);
+
+        HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+        Stream myResponseStream = response.GetResponseStream();
+        StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(encode));
+        string retString = myStreamReader.ReadToEnd();
+        return retString;
+    }
+    public static async Task<string> HttpGet(string url, string getDataStr = null, string encode = "utf-8") {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + (getDataStr == null ? "" : "?") + getDataStr);
+        request.Method = "GET";
+        request.ContentType = "text/html;charset=UTF-8";
+        HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+        Stream myResponseStream = response.GetResponseStream();
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(encode));
+        string retString = myStreamReader.ReadToEnd();
+        return retString;
+    }
+
+}
+class RegLib {
+    public static string RegexMatch(string regStr,ref string input) {
+        Regex reg = new Regex(regStr);
+        Match match = reg.Match(input);
+        return match.Value;
+    }
+}
---
+++ beiyou/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace beiyou
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        CampusNetwork campusNetwork;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            campusNetwork = new CampusNetwork();
+        }
+
+        private async void Login_Click(object sender, RoutedEventArgs e) {
+            Library.DebugOutput("student id " + StudentId.Text);
+            Library.DebugOutput("student passwd " + StudentPasswd.Text);
+            await campusNetwork.Login(StudentId.Text, StudentPasswd.Text);
+        }
+
+        private async void Logout_Click(object sender, RoutedEventArgs e) {
+            await campusNetwork.Logout();
+        }
+    }
+}
